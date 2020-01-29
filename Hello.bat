9762d935043e4e9a6992cee716e7dd71002939de
SanjayH2020 <60344431+SanjayH2020@users.noreply.github.com>
2020-01-29 11:22:29 +0530

    Update Hello.bat

--- Hello.bat
+++ Hello.bat
@@ -1,4 +1,3 @@
-echo "Hello World:
-      %date% :%time%"
-      echo "I am a Feature"
-      echo "I am a Defect Branch"
+echo "Hello World: %date% :%time%"
+echo "I am a Feature"
+echo "I am a Defect Branch"
